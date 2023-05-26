ee6c025961e018dfe3c3283dadb297d4fffe52a3
Asmit Khare <113673589+asmitkhare12@users.noreply.github.com>
2023-05-26 11:46:12 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import Home from "./pages/home";
-//import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return <Home />;
+  return (
+    <div className="App">
+      <div className="loginContainer">
+        <h1>Welcome Back!</h1>
+
+        <div className="input-container">
+          <label>Username </label>
+          <input type="text" name="uname" required />
+          
+        </div>
+        <div className="input-container">
+          <label>Password </label>
+          <input type="password" name="pass" required />
+          
+        </div>
+
+        <a href="#">Forgot password?</a>
+        <a href="#">Create a new account</a>
+
+        <button className="loginBut">
+          <p>Login</p>
+        </button>
+
+       
+      </div>
+    </div>
+  );
 }
 
 export default App;
